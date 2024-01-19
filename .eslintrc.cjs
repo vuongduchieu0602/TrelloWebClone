025b0060686dc500b7298d7e71f40234fdbc6da8
Vuong Duc Hieu <vg.duchieu0602@gmail.com>
2024-01-19 21:04:03 +0700

    Update Eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,44 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'plugin:react-hooks/recommended'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],  // 1
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: [
+    'react',
+    'react-hooks',
+    'react-refresh'
+  ],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+    'react-refresh/only-export-components': 'warn', //validate that your components can safely be updated with fast refresh
+    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+    "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
+    'react/prop-types': 0,
+    'react/display-name': 0,
+
+    'no-console': 1,
+    'no-lonely-if': 1,
+    'no-unused-vars': 1,
+    'no-trailing-spaces': 1,
+    'no-multi-spaces': 1,
+    'no-multiple-empty-lines': 1,
+    'space-before-blocks': ['error', 'always'],
+    'object-curly-spacing': [1, 'always'],
+    'indent': ['warn', 2], //tab size = 2
+    'semi': [1, 'never'], //don't have ;
+    'quotes': ['error', 'single'],
+    'array-bracket-spacing': 1,
+    'linebreak-style': 0, //window: \r\n but unix: \n
+    'no-unexpected-multiline': 'warn',
+    'keyword-spacing': 1,
+    'comma-dangle': 1,
+    'comma-spacing': 1,
+    'arrow-spacing': 1
+  }
 }
