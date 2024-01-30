69ed22c97ff14bd848f4436b883f33a33688da68
Vuong Duc Hieu <vg.duchieu0602@gmail.com>
2024-01-30 15:02:30 +0700

    Dark light mode #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { useColorScheme } from '@mui/material/styles'
+import Button from '@mui/material/Button'
 
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
   return (
     <>
-      Hello
+      <ModeToggle />
     </>
   )
 }
