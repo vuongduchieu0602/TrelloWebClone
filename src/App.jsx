cde97025d9c8fdd1bfa65dd251a285005d8da349
Vuong Duc Hieu <vg.duchieu0602@gmail.com>
2024-01-30 22:20:58 +0700

    Mode Select

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,41 @@
 import { useColorScheme } from '@mui/material/styles'
 import Button from '@mui/material/Button'
+import * as React from 'react'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
 
-function ModeToggle() {
+function ModeSelect() {
   const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    const modeSelect = event.target.value
+    setMode(modeSelect)
+  }
+
   return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-light-dark-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-light-dark-mode"
+        id="select-light-dark-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">Light</MenuItem>
+        <MenuItem value="dark">Dark</MenuItem>
+        <MenuItem value="system">System</MenuItem>
+      </Select>
+    </FormControl>
   )
 }
 
 function App() {
   return (
     <>
-      <ModeToggle />
+      <ModeSelect />
     </>
   )
 }
