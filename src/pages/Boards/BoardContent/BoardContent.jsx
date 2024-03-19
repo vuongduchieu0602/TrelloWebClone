583701bc67cb0affac711cf0c79fb9e3b23d47a3
Vuong Duc Hieu <vg.duchieu0602@gmail.com>
2024-03-20 00:10:08 +0700

    mock data

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,7 +1,7 @@
 import Box from '@mui/material/Box'
 import ListColumns from './List Columns/ListColumns'
 
-function BoardContent() {
+function BoardContent({ board }) {
   return (
     <Box sx={{
       bgcolor: 'primary.light',
@@ -11,7 +11,7 @@ function BoardContent() {
       backgroundColor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
       padding: '10px 0'
     }}>
-      <ListColumns />
+      <ListColumns columns={board?.columns}/>
     </Box>
   )
 }
